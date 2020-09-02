846eb4ca59c777ae6846af8fdfddb80085513746
Vasco Ramos <vascoalramos@outlook.pt>
2020-09-02 13:54:35 +0100

    Delete misplaced newline

--- environments/publish-local-plugins.sh
+++ environments/publish-local-plugins.sh
@@ -18,7 +18,6 @@ cd sbt-hbs && sbt publishLocal
 
 # add repositories into the file ~/.sbt/repositories
 echo "[repositories]
-
 local
 sbt-releases-repo: https://repo.typesafe.com/typesafe/ivy-releases/, [organization]/[module]/(scala_[scalaVersion]/)(sbt_[sbtVersion]/)[revision]/[type]s/[artifact](-[classifier]).[ext]
 sbt-plugins-repo: https://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/, [organization]/[module]/(scala_[scalaVersion]/)(sbt_[sbtVersion]/)[revision]/[type]s/[artifact](-[classifier]).[ext]
