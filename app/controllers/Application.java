61e4b6bbd6d96d973fefd815f1f497bd72f93222
Vasco Ramos <vascoalramos@outlook.pt>
2020-09-07 11:38:43 +0100

    Add endpoint to get list of users

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -42,17 +42,35 @@ public class Application extends Controller {
                 routes.javascript.Application.supportedLanguages(),
                 routes.javascript.HomeController.index(),
 
-                // Login controller
+                routes.javascript.SvgController.getImage(),
+
+                // Login Controller
                 controllers.login.routes.javascript.Login.index(),
-                controllers.login.routes.javascript.Login.login(), controllers.login.routes.javascript.Login.logout(), controllers.rbac.routes.javascript.Users.getLoggedInUser(),
+                controllers.login.routes.javascript.Login.login(),
+                controllers.login.routes.javascript.Login.logout(),
 
-//                routes.javascript.SvgController.getMiddleImage(),
-//                routes.javascript.SvgController.getSmallImage(),
-				routes.javascript.SvgController.getImage(),
+                // User Controller
+                controllers.rbac.routes.javascript.Users.getLoggedInUser(),
+				controllers.rbac.routes.javascript.Users.getAllUsers(),
 
-				routes.javascript.InstitutionController.getInstitutionName(), routes.javascript.InstitutionController.scheduler(), routes.javascript.InstitutionController.getInstitutions(), routes.javascript.InstitutionController.newInstitution(), routes.javascript.InstitutionController.newServer(), routes.javascript.InstitutionController.deleteServer(), routes.javascript.InstitutionController.getServer(), routes.javascript.InstitutionController.updateServer(), routes.javascript.InstitutionController.newDataSource(), routes.javascript.InstitutionController.deleteDataSource(), routes.javascript.InstitutionController.getDataSource(), routes.javascript.InstitutionController.updateDataSource(), routes.javascript.InstitutionController.getDataSources(), routes.javascript.InstitutionController.getComponents(),
+				// Institution Controller
+				routes.javascript.InstitutionController.getInstitutionName(),
+                routes.javascript.InstitutionController.scheduler(),
+                routes.javascript.InstitutionController.getInstitutions(),
+                routes.javascript.InstitutionController.newInstitution(),
+                routes.javascript.InstitutionController.newServer(),
+                routes.javascript.InstitutionController.deleteServer(),
+                routes.javascript.InstitutionController.getServer(),
+                routes.javascript.InstitutionController.updateServer(),
+                routes.javascript.InstitutionController.newDataSource(),
+                routes.javascript.InstitutionController.deleteDataSource(),
+                routes.javascript.InstitutionController.getDataSource(),
+                routes.javascript.InstitutionController.updateDataSource(),
+                routes.javascript.InstitutionController.getDataSources(),
+                routes.javascript.InstitutionController.getComponents(),
                 routes.javascript.InstitutionController.getSchedules(),
 
+                // Trans Graph Controller
                 routes.javascript.TransGraphController.getTaskDetails(),
                 routes.javascript.TransGraphController.newTask(),
                 routes.javascript.TransGraphController.deleteTask(),
@@ -69,6 +87,7 @@ public class Application extends Controller {
                 routes.javascript.TransGraphController.getExecutions(),
                 routes.javascript.TransGraphController.getServers(),
 
+                // Step Controller
                 routes.javascript.StepController.configure(),
                 routes.javascript.StepController.showStepInput(),
                 routes.javascript.StepController.showStepOutput(),
@@ -86,15 +105,14 @@ public class Application extends Controller {
                 routes.javascript.StepController.getConditionValue(),
                 routes.javascript.StepController.getMultiSelects(),
                 routes.javascript.StepController.getInstitution(),
-
                 routes.javascript.StepController.getByComponentAndShortName(),
                 routes.javascript.StepController.getMetadataByComponentAndShortName(),
 
+                // Execution Controller
                 routes.javascript.ExecutionController.logs(),
                 routes.javascript.ExecutionController.metrics(),
                 routes.javascript.ExecutionController.previewData(),
                 routes.javascript.ExecutionController.previewStep(),
-
                 routes.javascript.ExecutionController.localExecution(),
                 routes.javascript.ExecutionController.remoteExecution(),
                 routes.javascript.ExecutionController.result(),
